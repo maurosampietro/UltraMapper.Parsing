5b935d88323725256a0ea09c2475c35a075d1407
mauro sampietro <mauro.sampietro@gmail.com>
2022-07-04 22:01:52 +0200

    params update

--- UltraMapper.Parsing/Parameters/ComplexParam.cs
+++ UltraMapper.Parsing/Parameters/ComplexParam.cs
@@ -1,12 +1,17 @@
-﻿namespace UltraMapper.Parsing
+﻿using System;
+using System.Collections.Generic;
+using UltraMapper.Internals;
+
+namespace UltraMapper.Parsing
 {
     public class ComplexParam : IParsedParam
     {
         public string Name { get; set; }
         public int Index { get; set; }
-        public IParsedParam[] SubParams { get; set; }
 
-        public IParsedParam this[ int index ] 
+        public IList<IParsedParam> SubParams { get; set; }
+
+        public IParsedParam this[ int index ]
             => this.SubParams[ index ];
     }
 }
